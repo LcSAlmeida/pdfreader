aa3270649ffd4695ba6a3f9fc3665fc4bc3cd132
Lucas Almedia <89208365+LcSAlmeida@users.noreply.github.com>
2024-11-10 18:02:03 -0300

    first commit

---
+++ src/main/java/com/coy/pdfreader/PdfreaderApplication.java
@@ -0,0 +1,13 @@
+package com.coy.pdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfreaderApplication.class, args);
+	}
+
+}
